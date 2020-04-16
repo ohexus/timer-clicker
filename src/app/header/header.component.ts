69acf77f29af21cc5fd44b9ba081f1f2d9c59619
ohexus <aleksversen@gmail.com>
2020-04-16 23:21:45 +0300

    added font, start screen styling and check for empty input

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,11 +7,26 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 })
 export class HeaderComponent {
   title: string = 'Timer Clicker Game'
+  isWarningShow: boolean = false
 
   @Input() fullscreen: boolean
   @Output() newNameEvent = new EventEmitter()
 
   emitNewName(value: string) {
     this.newNameEvent.emit(value)
+  }
+
+  checkName(value: string) {
+    const name = value.trim()
+
+    if (name) {
+      this.newNameEvent.emit(name)
+    } else {
+      this.isWarningShow = true
+
+      setTimeout(() => {
+        this.isWarningShow = false
+      }, 3000)
+    }
   }
 }
