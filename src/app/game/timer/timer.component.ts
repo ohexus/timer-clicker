1647ce5160300f5581db6f4037dd38588a25e7f4
ohexus <aleksversen@gmail.com>
2020-04-19 01:52:48 +0300

    added timer styling

--- src/app/game/timer/timer.component.ts
+++ src/app/game/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core'
+import { Component, Output, EventEmitter, HostListener } from '@angular/core'
 
 @Component({
   selector: 'app-timer',
@@ -9,46 +9,75 @@ export class TimerComponent {
   timerTotalLength: number = 10
   timerCurrentValue: number = this.timerTotalLength
 
-  timerStep: number = 0.1
+  widthMultiplier: number = 2
+  timerWidth: string = `${this.timerTotalLength * this.widthMultiplier}rem`
 
   isTimerStarted: boolean = false
 
   @Output() timerEndEvent = new EventEmitter()
-  @Output() timerLengthEvent = new EventEmitter()
+  @Output() timerLengthChangeEvent = new EventEmitter()
+
+  @HostListener('window:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (!this.isTimerStarted) {
+      const k = event.key
+  
+      if (k === 'w' || k === 'd' || k === 'ArrowUp' || k === 'ArrowRight') {
+        this.increaseTimerLength()
+      }
+  
+      if (k === 'a' || k === 's' || k === 'ArrowLeft' || k === 'ArrowDown') {
+        this.decreaseTimerLength()
+      }
+    }
+  }
 
   emitTimerEnd() {
     this.timerEndEvent.emit()
     this.timerCurrentValue = this.timerTotalLength
   }
 
-  emitTimerLength() {
-    this.timerLengthEvent.emit(this.timerTotalLength)
+  emitTimerLengthChange() {
+    this.timerLengthChangeEvent.emit(this.timerTotalLength)
   }
 
   increaseTimerLength() {
     this.timerTotalLength++
+
     this.timerCurrentValue = this.timerTotalLength
-    this.emitTimerLength()
+    this.changeTimerWidth()
+
+    this.emitTimerLengthChange()
   }
-  
+
   decreaseTimerLength() {
-    this.timerTotalLength--
-    this.timerCurrentValue = this.timerTotalLength
-    this.emitTimerLength()
+    if (this.timerTotalLength > 1) {
+      this.timerTotalLength--
+
+      this.timerCurrentValue = this.timerTotalLength
+      this.changeTimerWidth()
+
+      this.emitTimerLengthChange()
+    }
   }
 
   startTimer() {
     this.isTimerStarted = true
+    const timerStep: number = 0.1
 
     let timerId = setInterval(() => {
-      this.timerCurrentValue -= this.timerStep
+      this.timerCurrentValue -= timerStep
+
+      this.changeTimerWidth()
 
       if (this.timerCurrentValue < 0) {
         this.isTimerStarted = false
+        this.timerWidth = this.multiplyTimerWidth(this.timerTotalLength).toString() + 'rem'
+
         this.emitTimerEnd()
         clearInterval(timerId)
       }
-    }, this.timerStep * 1000)
+    }, timerStep * 1000)
   }
 
   convertTime(timeInSeconds: number) {
@@ -59,7 +88,9 @@ export class TimerComponent {
     return `${format(minutes, 2)}:${format(seconds, 2)}`
   }
 
-  getTimerRangePosition() {
-    return (this.timerCurrentValue * 100) / this.timerTotalLength
+  changeTimerWidth() {
+    this.timerWidth = this.multiplyTimerWidth(this.timerCurrentValue).toString() + 'rem'
   }
+
+  multiplyTimerWidth = (value) => value * this.widthMultiplier
 }
